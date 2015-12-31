e2cf41821226db2f23546e7c85236ee723df268a
jimtoepel <jim.toepel@gmail.com>
2015-12-31 15:12:45 -0800

    Set up views
    Crashes, needs info for number of rows.

---
+++ TaskManager.swift
@@ -0,0 +1,27 @@
+//
+//  TaskManager.swift
+//  IntentlySwift
+//
+//  Created by Jim Toepel on 12/31/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+import UIKit
+
+var taskMgr: TaskManager = TaskManager()
+
+struct task {
+    var name = "Name"
+    var desc = "Decription"
+}
+
+class TaskManager: NSObject {
+    var taskList = [task]()
+    
+    func addTask(name:String, desc: String){
+        taskList.append(task(name:name, desc: desc))
+    }
+    
+    
+}
+
